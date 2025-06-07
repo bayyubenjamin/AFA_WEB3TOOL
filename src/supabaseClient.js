a0dc93b30639f18fea4a2dee997e75936baaefbe
bayyubenjamin <69312778+bayyubenjamin@users.noreply.github.com>
2025-06-07 17:56:54 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://cxoykbwigsfheaegpwke.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4b3lrYndpZ3NmaGVhZWdwd2tlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4NzcxMzUsImV4cCI6MjA2NDQ1MzEzNX0.yooMcQ6e2CSFdnNm9ogOiLrc8i09LS2MUNdI8WzNy8c';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
