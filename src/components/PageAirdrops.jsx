0556ceb47622f652e4c680b856f1d893b46b35bf
bayyubenjamin <69312778+bayyubenjamin@users.noreply.github.com>
2025-06-08 17:28:40 +0700

    Update PageAirdrops.jsx

--- src/components/PageAirdrops.jsx
+++ src/components/PageAirdrops.jsx
@@ -15,7 +15,7 @@ import { supabase } from '../supabaseClient';
 import AirdropAdminForm from './AirdropAdminForm';
 
 // GANTI DENGAN USER ID ADMIN KAMU YANG ASLI
-const ADMIN_USER_ID = 'GANTI_DENGAN_USER_ID_ADMIN_KAMU'; 
+const ADMIN_USER_ID = '9a405075-260e-407b-a7fe-2f05b9bb5766'; 
 
 const getTranslations = (lang) => (lang === 'id' ? translationsId : translationsEn);
 
