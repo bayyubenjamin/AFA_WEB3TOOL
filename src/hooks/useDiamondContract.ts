2a801f517be8ba2839c3abc65f50f0c681c989a4
Bayu Benjamin <bayyujack@gmail.com>
2025-06-25 02:23:07 +0700

    update page identity

--- src/hooks/useDiamondContract.ts
+++ src/hooks/useDiamondContract.ts
@@ -3,7 +3,7 @@ import { useMemo } from "react";
 import { ethers } from "ethers";
 import DiamondABI from "../contracts/AFAIdentityDiamondABI.json";
 
-const DIAMOND_ADDRESS = "0xd9aB239C897A1595df704124c0bD77560CA3655F";
+const DIAMOND_ADDRESS = "0x102dA6B04621114dc25CB1dcc0C3e7EF70678c93";
 
 export function useDiamondContract() {
   const provider = useMemo(() => new ethers.providers.Web3Provider(window.ethereum), []);
