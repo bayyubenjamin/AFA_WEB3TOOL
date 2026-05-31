d77869a9d71186ece2132b7247394fa58194555d
Bayyu Benjamin <bayyujack@gmail.com>
2026-05-31 07:35:44 +0700

    fix: correct smart contract access control modifier

--- src/services/telemetry-9.js
+++ src/services/telemetry-9.js
@@ -1,8 +1,8 @@
 /**
- * Telemetry updated at 2026-05-28T08:15:28Z
+ * Telemetry updated at 2026-05-31T00:35:44Z
  * @param {string} eventName
  */
-export const logEvent_5 = (eventName) => {
+export const logEvent_79 = (eventName) => {
   if (!eventName) return;
   console.debug('Event triggered:', eventName);
 };
